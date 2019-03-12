fcdb8140279aa0b84c239cbc76295b076e2b50df
Wendy Hamilton <wdhamilton117@gmail.com>
2019-03-12 21:06:07 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,28 @@
+class CashRegister
 
+@items = []
+
+def initialize
+  @total = 0
+end
+
+
+def total
+  @total
+end
+
+def add_item
+end
+
+def apply_discount
+end
+
+def items
+  @items
+end
+
+def void_last_transaction
+end
+
+
+end
