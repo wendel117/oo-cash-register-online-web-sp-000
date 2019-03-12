add7aadb3a3e0cd5d7e90afd72a6632250c24f9d
Wendy Hamilton <wdhamilton117@gmail.com>
2019-03-12 21:09:40 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,10 @@
 class CashRegister
 
-attr_accessor :total :discount
+attr_accessor :total, :discount
 
-def initialize
+def initialize(discount)
   @total = 0
+  @discount = discount
 end
 
 def total
